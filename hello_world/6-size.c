a2aa4ab4cd175299591b1ab74183d7b8beda0f64
Gumboy31 <theo.jennat98@gmail.com>
2023-10-13 05:22:07 -0700

    size printf command

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of a int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
