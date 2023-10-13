e88c022dcf3912c34ad02e67f1c4b9ffe3ee7c6d
Gumboy31 <theo.jennat98@gmail.com>
2023-10-13 04:34:39 -0700

    print f using up

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void)I
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
