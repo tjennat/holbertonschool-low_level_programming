be7bc70b277656c14183b36c3fd47221be212d3c
Gumboy31 <theo.jennat98@gmail.com>
2023-10-13 04:39:37 -0700

    printing with printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)I
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
