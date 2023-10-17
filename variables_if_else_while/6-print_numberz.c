31217b1ce645fa934c0a4e613c620640c162f870
tjennat <theo.jennat98@gmail.com>
2023-10-17 06:51:32 -0700

    printing numbers with pchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Printing numberz 
+ * Return: 0 to sucess
+ */
+int main(void)
+{
+	int digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
