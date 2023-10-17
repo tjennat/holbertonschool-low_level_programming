d1c087894eedd95fbf75d53a69bf521c927b2f5c
tjennat <theo.jennat98@gmail.com>
2023-10-17 05:18:42 -0700

    Printing alphabets with pchar in lower follow by upper

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - charput and char to recreate alpha in lower followed by upper
+ * Return: 0 to sucess
+ */
+int main(void)
+{
+	char alphabet = 'a';
+		while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+{
+	char ALPHABET = 'A';
+		while (ALPHABET <= 'Z')
+		{
+		putchar(ALPHABET);
+		ALPHABET++;
+		}
+	putchar('\n');
+	return (0);
+}}
