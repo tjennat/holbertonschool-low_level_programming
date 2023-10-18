94ce3940d48223deee4e48043dfcc1cacaff6b0f
tjennat <theo.jennat98@gmail.com>
2023-10-18 08:09:46 -0700

    priting alphabet nested loop

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,23 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Using charput and char to recreate alphabet in lowercase
+ * print_alphabet_x10 - Using charput to alphabet x10
  * Return: alphabet to sucess
  */
 void print_alphabet_x10(void)
 {
 	int a = 0;
+
 	while (a < 10)
 	{
+
 		char alphabet = 'a';
+
 		while (alphabet <= 'z')
 		{
 			putchar(alphabet);
 			alphabet++;
-		}	
+		}
 		putchar('\n');
 		a++;
 	}}
