7bedf6aa0960d4d64ef042935cdd4a197a864b7c
tjennat <theo.jennat98@gmail.com>
2023-10-19 06:17:02 -0700

    printing sign function

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - entry point of printing sign of a number
+ * @n: is my var
+ * Return: to sucess
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (0);
+}
