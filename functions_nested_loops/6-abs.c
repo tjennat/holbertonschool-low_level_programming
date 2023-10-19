a079380f2bad1c4d7f0204379242e6eb345e0822
tjennat <theo.jennat98@gmail.com>
2023-10-19 06:41:46 -0700

    abs absolute function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - absolute number
+ * @n: is my var
+ * Return: to sucess
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
