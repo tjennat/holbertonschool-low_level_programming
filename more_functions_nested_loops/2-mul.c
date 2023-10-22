f5fdcc2687bc327ca84ad96c6361ae74b8f33005
tjennat <theo.jennat98@gmail.com>
2023-10-22 06:57:17 -0700

    function that multiplicate

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - is basically a multiplication
+ * @a: is my var that's going to be multiplied by b
+ * @b: is my var that's going to be multiplied by a
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+			return (a * b);
+}
