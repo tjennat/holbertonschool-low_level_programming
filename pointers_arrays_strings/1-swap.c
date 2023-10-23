af86f5ca219a4e0ff5a8012dd30aaadeb111fd01
tjennat <theo.jennat98@gmail.com>
2023-10-23 06:50:14 -0700

    swapping with a temporary var

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping
+ * @a: is the first var I want to switch
+ * @b: is the second var I want to switch with
+ */
+void swap_int(int *a, int *b)
+{
+	int t;
+
+	*a = t;
+	*a = *b;
+	*b = t;
+}
