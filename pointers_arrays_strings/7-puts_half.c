4120251295f2b434d8c377ed550bae87ddf92021
tjennat <theo.jennat98@gmail.com>
2023-10-25 03:44:44 -0700

    rename of my var

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,21 +8,21 @@
 void puts_half(char *str)
 {
 	int length = 0;
-	int i, start;
+	int i, n;
 
 	while (str[length] != '\0')
 	{
 		length++;
 	}
 
-	start = length / 2;
+	n = length / 2;
 
 	if (length % 2 != 0)
 	{
-		start = (length - 1) / 2;
+		n = (length - 1) / 2;
 	}
 
-	for (i = start; i < length; i++)
+	for (i = n; i < length; i++)
 	{
 		_putchar(str[i]);
 	}
