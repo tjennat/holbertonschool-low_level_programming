45433e3e10ee06f766288329fa52adda6d868cb3
tjennat <theo.jennat98@gmail.com>
2023-10-25 14:01:30 -0700

    function strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copy a string
+ * @dest: buffer pointer
+ * @src: is my pointer
+ * Return: pointer to the string dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
