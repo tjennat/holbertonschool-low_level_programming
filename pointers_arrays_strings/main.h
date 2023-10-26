9225d071b8d510323f962493dde537a4199aeb5f
tjennat <theo.jennat98@gmail.com>
2023-10-26 11:29:12 -0700

    main.h update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void rev_string(char *s);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
 #endif
