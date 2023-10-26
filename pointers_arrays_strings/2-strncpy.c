d02568ac9b048dacac3721f043ec589f6c3b9661
tjennat <theo.jennat98@gmail.com>
2023-10-26 14:35:10 -0700

    copying string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copying a string
+ * @dest: string dest
+ * @src: string src
+ * @n: number of element
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	
+	return dest;
+}
