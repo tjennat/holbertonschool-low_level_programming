37999808872c960f4b3744064f03f08d14265b96
tjennat <theo.jennat98@gmail.com>
2023-10-26 07:25:35 -0700

    function strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * 
- *
- *
+ * _strcat - function that apprends two strings
+ * @dest: dest string
+ * @src: src string
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i
-	int 
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
