e773005ea67cb5db36338e002f2be0c37c5f05f3
tjennat <theo.jennat98@gmail.com>
2023-10-26 14:32:54 -0700

    adding up main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,5 +14,5 @@ int _atoi(char *s);
 void rev_string(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
 #endif
