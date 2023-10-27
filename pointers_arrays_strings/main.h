ee8f2a4062c058491ec9d9814b2baf73bf3fe975
tjennat <theo.jennat98@gmail.com>
2023-10-27 02:01:29 -0700

    main.h hotfix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ void rev_string(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 #endif
