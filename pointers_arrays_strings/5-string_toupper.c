85d7ea5fc36f6decfaf25b9dfe7c3f42fdfbb0d5
tjennat <theo.jennat98@gmail.com>
2023-10-27 06:31:28 -0700

    string rev

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - string
+ * @i: elements
+ * @s: pointer to the string
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
