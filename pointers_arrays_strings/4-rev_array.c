ea313a12b6b5156e0df902a7f9312f87ea9e092e
tjennat <theo.jennat98@gmail.com>
2023-10-27 04:48:15 -0700

    reversing my array and printing elements

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing number element 
+ * @a: pointer to the string
+ * @n: numb of elements
+ */
+void reverse_array(int *a, int n)
+{
+	int rev = 0;
+	int ver = n - 1;
+	int tmp;
+
+	while (rev < ver)
+	{
+		tmp = a[rev];
+		a[rev] = a[ver];
+		a[ver] = tmp;
+		rev++;
+		ver--;
+	}
+}
