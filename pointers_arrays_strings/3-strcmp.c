e65c5c224980a818b0e010e84eb5dc0e91253d17
tjennat <theo.jennat98@gmail.com>
2023-10-27 02:05:32 -0700

    comparing two strings with ASCII up

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int res = (s1[i] - s2[i]);
 
-		while (s1[i] != '\0' && s2[i] != '\0')
-		{
-			if (s1[i] != s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
 			return (res);
-				i++;
-		}
+		i++;
+	}
 	return (0);
 }
