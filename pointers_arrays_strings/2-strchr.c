d20599617a6de1355c20d87bd370e043249eae11
tjennat <theo.jennat98@gmail.com>
2023-10-28 02:38:21 -0700

    function strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - finding a char in a string
+ * @s: point to src source memory area
+ * @c: point to source memory area
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
+
