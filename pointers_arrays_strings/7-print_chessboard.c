9b72d271d1471e08d4c66c1c96b8650fda33445b
tjennat <theo.jennat98@gmail.com>
2023-10-29 09:40:25 -0700

    printing a chessboard with C

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Printing an chessboard
+ * @a: is var representing my array
+ */
+void print_chessboard(char (*a)[8])
+{
+    int row, col;
+
+    for (row = 0; row < 8; row++)
+    {
+        for (col = 0; col < 8; col++)
+        {
+            _putchar(a[row][col]);
+        }
+        _putchar('\n');
+    }
+}
+
