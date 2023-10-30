7e862fefb0a2e87d593ca0cd6143fad4dea15764
tjennat <theo.jennat98@gmail.com>
2023-10-30 08:16:19 -0700

    Function that returns factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - using factorial in recursive
+ * @n: is my var
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
