19d9260764dadbb1989b3e9d54934157d0d571a5
tjennat <theo.jennat98@gmail.com>
2023-10-30 10:30:42 -0700

    using power with recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to returns value of power with recursion
+ * @x: is the var I want to raise to
+ * @y: is the var I want to be raise to
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+	{
+		return (1);
+	}
+		return (x * _pow_recursion(x, y - 1));
+}
