a02a19854b873bdef6a56360cac0f7e13bbefe12
tjennat <theo.jennat98@gmail.com>
2023-10-30 06:02:33 -0700

    function puts with recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function puts but in recursive
+ * @s: is pointer to the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
