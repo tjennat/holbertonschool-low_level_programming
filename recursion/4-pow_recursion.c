7ccbb2396545b999695fd7c06c397de52d0753a5
tjennat <theo.jennat98@gmail.com>
2023-11-01 01:18:39 -0700

    adding return

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,6 +4,7 @@
  * _pow_recursion - function to returns value of power with recursion
  * @x: is the var I want to raise to
  * @y: is the var I want to be raise to
+ * Return: x^y
  */
 int _pow_recursion(int x, int y)
 {
