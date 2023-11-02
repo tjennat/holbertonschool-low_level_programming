c9f031c2629d6d9a85d1cb7cd78fe105f16fe138
tjennat <theo.jennat98@gmail.com>
2023-11-02 03:40:53 -0700

    printf function with argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - printing name with argc argv
+ * @argc: argu var
+ * @argv: argu var
+ * Return: zero
+ */
+int main(int argc, char ** argv)
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
