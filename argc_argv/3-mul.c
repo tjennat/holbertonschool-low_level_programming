7c64bc2e81621082b10ea4a99dc34bacdb2b7318
tjennat <theo.jennat98@gmail.com>
2023-11-02 08:17:29 -0700

    multiplicating with arg up

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,9 +12,9 @@ int main(int argc, char **argv)
 	int j = 1;
 
 
-	if (argc > 1)
+	if (argc >= 3)
 	{
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		j = j * atoi(argv[i]);
@@ -22,8 +22,8 @@ int main(int argc, char **argv)
 	printf("%d\n", j);
 	}
 
-	if (argc == 1)
+	if (argc < 3)
 		printf("Error\n");
-	
+
 	return (0);
 }
