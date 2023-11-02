e2b10da5ef6f9b95c20ecdc473f31de040b1507a
tjennat <theo.jennat98@gmail.com>
2023-11-02 06:14:13 -0700

    number of argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - printing number of arg with argc
+ * @argc: argu var
+ * @argv: argu var
+ * Return: zero
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
