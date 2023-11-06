9cd6f02c66ad3416aa9fbac5119bed1adf5c7952
tjennat <theo.jennat98@gmail.com>
2023-11-06 05:03:15 -0800

    creating array malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create _array - creating an array calculating
+ * @size: size of the array
+ * @c: is my char that's going to point
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = (char *)malloc(size * sizeof(char));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c; 
+	return (arr);
+}
