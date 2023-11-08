fcdb43d5ab4cd3ba86fa7200c1510e1399d88a85
tjennat <theo.jennat98@gmail.com>
2023-11-08 01:11:26 -0800

    up header

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -7,5 +5,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int _putchar(char c);
 #include <stddef.h>
+#include <string.h>
 #include <stdlib.h>
-#endif
