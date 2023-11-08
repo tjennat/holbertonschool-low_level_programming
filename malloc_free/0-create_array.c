3fb097bd29967250d8832fb89d13438c03160009
tjennat <theo.jennat98@gmail.com>
2023-11-08 01:05:11 -0800

    creating array up

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,9 +17,10 @@ char *create_array(unsigned int size, char c)
 	arr = (char *)malloc(size * sizeof(char));
 
 	if (arr == NULL)
+	{
 		free(arr);
 		return (NULL);
-
+	}
 	for (i = 0; i < size; i++)
 		arr[i] = c; 
 	return (arr);
