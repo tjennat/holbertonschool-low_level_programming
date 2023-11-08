406d407829b805390cc9df8c2e1789302ed6b072
tjennat <theo.jennat98@gmail.com>
2023-11-08 01:15:42 -0800

    rm char*

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = (char *)malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char);
 
 	if (arr == NULL)
 	{
