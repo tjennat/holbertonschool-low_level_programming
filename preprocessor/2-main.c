9932c73aabe680847e031ae6ced802f68b64d8c1
tjennat <theo.jennat98@gmail.com>
2023-11-09 13:48:08 -0800

    file name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that print the name of the file
+ * Return: zero
+ */
+int main(void)
+{
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
+}
+
