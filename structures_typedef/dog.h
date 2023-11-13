e10b939bd3ed465bcda05cabf6c67b8506dfab01
tjennat <theo.jennat98@gmail.com>
2023-11-13 07:28:56 -0800

    dog under t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,14 +9,13 @@
  * @owner: is me
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
-typedef struct dog_t
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
