4f98828c89d1db2f69e6934aa64750881a672245
tjennat <theo.jennat98@gmail.com>
2023-11-13 04:51:28 -0800

    typedef is solution

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,8 +2,6 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 /**
  * struct dog - is my dog
@@ -12,10 +10,13 @@ void print_dog(struct dog *d);
  * @owner: is me
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
