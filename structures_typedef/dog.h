32eb125f2afcad9662003ff50598ec81e3379ad5
tjennat <theo.jennat98@gmail.com>
2023-11-13 02:23:24 -0800

    update of my header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 /**
  * struct dog - is my dog
