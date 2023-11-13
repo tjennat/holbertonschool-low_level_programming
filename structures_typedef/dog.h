86002153c1a355685b8d33fe4b2380ab918b586b
tjennat <theo.jennat98@gmail.com>
2023-11-13 01:57:27 -0800

    dog header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is my dog
+ * @name: is the name of my dog
+ * @age: is old bro
+ * @owner: is me
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
