1973c51de4a5ba5c9231b52fac5d811f20f1f7b3
tjennat <theo.jennat98@gmail.com>
2023-11-15 00:32:57 -0800

    printing a name with pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - printing a name with pointer to a function
+ * @name: is the string
+ * @f: is my pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (*f != NULL)
+		(*f)(name);
+}
