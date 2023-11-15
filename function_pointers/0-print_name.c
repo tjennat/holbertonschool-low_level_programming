4ccb7ff83597fb5797480d7ef7c1a21e17497060
tjennat <theo.jennat98@gmail.com>
2023-11-15 00:45:42 -0800

    printing name with func to point

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
-#include "function_pointers.h"
 #include <stddef.h>
+#include "function_pointers.h"
+
 
 /**
  * print_name - printing a name with pointer to a function
@@ -8,6 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (*f != NULL)
-		(*f)(name);
+	if (f != NULL)
+		f(name);
 }
