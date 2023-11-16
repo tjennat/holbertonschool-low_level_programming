86de21ed3ce09a2c752bd8c7cd5a0475b3c27335
tjennat <theo.jennat98@gmail.com>
2023-11-16 13:38:47 -0800

    head up

--- variadic_function/variadic_functions.h
+++ variadic_function/variadic_functions.h
@@ -1,6 +1,5 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
-int _putchar(char c)
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
