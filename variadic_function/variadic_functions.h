8a03372b3350fbccf0efaebda34d7d868e820363
tjennat <theo.jennat98@gmail.com>
2023-11-16 13:15:35 -0800

    header var fun

---
+++ variadic_function/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+#include <stdlib.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
