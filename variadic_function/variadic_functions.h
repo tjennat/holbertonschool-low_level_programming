04f16a50d85d14df37ad1b60d474259a80c45928
tjennat <theo.jennat98@gmail.com>
2023-11-16 13:32:58 -0800

    header with putchar

--- variadic_function/variadic_functions.h
+++ variadic_function/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
-#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
