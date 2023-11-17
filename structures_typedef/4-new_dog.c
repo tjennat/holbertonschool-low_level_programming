5777af157164095c2a728eef857b62faca0042fc
tjennat <theo.jennat98@gmail.com>
2023-11-17 01:34:27 -0800

    final version of newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
-#include <string.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 /**
  * _strdup - duplicate a string of char
@@ -12,52 +11,52 @@ char *_strdup(char *str);
 
 char *_strdup(char *str)
 {
-	char *s;
+    char *s;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	s = malloc(strlen(str) + 1);
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-	{
-		strcpy(s, str);
-	}
-	return (s);
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    s = malloc(strlen(str) + 1);
+    if (s == NULL)
+    {
+        return (NULL);
+    }
+    {
+        strcpy(s, str);
+    }
+    return (s);
 }
+
 /**
- * new_dog - creating a new dog
- * @name: name is ouaoua
- * @age: is old bro
- * @owner: is me
- * Return: new dog or NULL
+ * dog_t *new_dog - creates a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ * Return: pointer to new dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ouaoua;
+    dog_t *new_dog;
 
-	ouaoua = malloc(sizeof(dog_t));
-	if (ouaoua == NULL)
-		return (NULL);
-
-	ouaoua->name = strdup(name);
-	if (ouaoua->name == NULL)
-	{
-		free(ouaoua);
-		return (NULL);
-	}
-	ouaoua->owner = strdup(owner);
-	if (ouaoua->owner == NULL)
-	{
-		free(ouaoua->name);
-		free(ouaoua);
-		return (NULL);
-	}
-
-	ouaoua->age = age;
-
-	return (ouaoua);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
+    new_dog->name = _strdup(name);
+    if (new_dog->name == NULL)
+    {
+        free(new_dog);
+        return (NULL);
+    }
+    new_dog->owner = _strdup(owner);
+    if (new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
+    new_dog->age = age;
+    return (new_dog);
 }
