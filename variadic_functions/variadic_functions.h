ca93ea88c3aa7f930502decc1b1757dfdf56a9e6
tjennat <theo.jennat98@gmail.com>
2023-11-17 11:00:06 -0800

    struct adding + stdarg

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,16 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct formats
+{
+	char *type;
+	void (*function)(va_list);
+} checker;
+
 #endif
