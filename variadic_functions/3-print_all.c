1ffb615129e40dd5f11a7b26a09b2ad95079ba80
tjennat <theo.jennat98@gmail.com>
2023-11-17 11:00:40 -0800

    printing all variadic func

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,55 +1,88 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
- * print_all - Prints anything.
- * @format: A list of types of arguments passed to the function.
- *           c: char
- *           i: integer
- *           f: float
- *           s: char * (if the string is NULL, print (nil) instead)
- *           Any other char should be ignored.
+ * print_char - fct print only char
+ * @list: args from
+ */
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_int - fct print only int
+ * @list: args from
+ */
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_float - fct print only float
+ * @list: args from
+ */
+
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_string - fct print only string
+ * @list: args from
+ */
+void print_string(va_list list)
+{
+	char *str = va_arg(list, char*);
+
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - function that prints anything.
+ * @format: what type to print
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char c;
+	int index1 = 0;
+	int index2 = 0;
+	char *str = "";
 
-    va_start(args, format);
+	va_list list;
 
-    while (format && format[i])
-    {
-        c = format[i];
+	checker storage[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{'\0', NULL}
+	};
 
-        if (i > 0)
-            printf(", ");
+	va_start(list, format);
 
-        if (c == 'c')
-        {
-            printf("%c", va_arg(args, int));
-        }
-        else if (c == 'i')
-        {
-            printf("%d", va_arg(args, int));
-        }
-        else if (c == 'f')
-        {
-            printf("%f", va_arg(args, double));
-        }
-        else if (c == 's')
-        {
-            str = va_arg(args, char *);
-            if (str == NULL)
-                printf("(nil)");
-            else
-                printf("%s", str);
-        }
+	while (format != NULL && format[index1] != '\0')
+	{
+		while (index2 < 4)
+		{
+			if (format[index1] == *storage[index2].type)
+			{
+				printf("%s", str);
+				storage[index2].function(list);
+				str = ", ";
+			}
+			index2++;
+		}
+		index1++;
+		index2 = 0;
+	}
+	printf("\n");
 
-        i++;
-    }
-
-    va_end(args);
-    printf("\n");
+	va_end(list);
 }
