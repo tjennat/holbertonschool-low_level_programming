57eca66dedd1274b344b1d8ae1f8b0aefcc9b6a3
tjennat <theo.jennat98@gmail.com>
2023-11-27 04:49:37 -0800

    printing all elements from singly linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * size_t print_list - will print all elements of a list 
+ * @h: is my pointer to the singly linked list
+ * return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nod = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		nod++;
+	}
+
+	return (nod);
+}
