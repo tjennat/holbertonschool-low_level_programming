e8083bc6860ac48c93ba408c22fca1f9229cadf9
tjennat <theo.jennat98@gmail.com>
2023-11-28 05:04:13 -0800

    adding proto to free

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,4 +21,5 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
