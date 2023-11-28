ff0188dcc21906a3c0026211f725a0b22d09a04d
tjennat <theo.jennat98@gmail.com>
2023-11-28 05:03:49 -0800

    missing blank line

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,6 +11,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
@@ -33,6 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		list_t *tmp = *head;
+
 		while (tmp->next != NULL)
 		{
 			tmp = tmp->next;
