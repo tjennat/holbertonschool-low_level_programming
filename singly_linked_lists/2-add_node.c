acb9745cda9cf748e14283aa5b54718b4f116c39
tjennat <theo.jennat98@gmail.com>
2023-11-28 02:47:12 -0800

    function that add a node and also duplicate the string

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - Adds a new node at the beginning of the list
+ * @head: A pointer to the pointer that point to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
+ * Return: address of my new node or NULL.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	new_node = malloc(sizeof(list_t));
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
