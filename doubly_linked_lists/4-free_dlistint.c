c10e67dda3b25adecaca00dcf1f208cf2f2d4a19
tjennat <theo.jennat98@gmail.com>
2023-11-29 06:24:16 -0800

    freeing doubly linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees my doubly linked list
+ * @head: is my pointer head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
