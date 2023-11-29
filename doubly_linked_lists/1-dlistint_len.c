83cd93c4b2faf734b9671052a4838e7e894924e5
tjennat <theo.jennat98@gmail.com>
2023-11-29 04:16:04 -0800

    betty requirement

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,14 +2,14 @@
 
 /**
  * dlistint_len - number of element in doubly linked list
- * @h: is the pointer to my doubly linked list 
+ * @h: is the pointer to my doubly linked lis
  * Return: num of elem or NULL
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int nod = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
 		nod++;
