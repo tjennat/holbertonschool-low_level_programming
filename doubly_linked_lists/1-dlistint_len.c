41380c4d411b0ed31141d4003ceb7b2f7aae14d7
tjennat <theo.jennat98@gmail.com>
2023-11-29 03:25:55 -0800

    number of element in my doubly linked list up description

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * dlistint_len - number of element in doubly linked list
- * @h: 
+ * @h: is the pointer to my doubly linked list 
  * Return: num of elem or NULL
  */
 size_t dlistint_len(const dlistint_t *h)
