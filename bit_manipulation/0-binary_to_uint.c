45c08eeb63fa86a18b72f719477e1a654cd658bc
tjennat <theo.jennat98@gmail.com>
2023-11-30 06:27:22 -0800

    function that convert binary to int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
-			return 0;
+			return (0);
 
 		result = (result << 1) + (*b - '0');
 		b++;
 	}
 
-	return result;
+	return (result);
 }
