d6dae5ca3fb6a569c9a66b7e27aafa0840731a5a
tjennat <theo.jennat98@gmail.com>
2023-11-30 12:42:28 -0800

    betty correction parenthesis wrong formulation

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,9 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-        return -1;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-    return (n >> index) & 1;
+	return ((n >> index) & 1);
 }
 
