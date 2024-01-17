a9273b18837385eee1eda5a60edbba5fcb6a3976
tjennat <theo.jennat98@gmail.com>
2024-01-17 13:57:00 +0100

    header of the dir

---
+++ search_algorithms/search.h
@@ -0,0 +1,10 @@
+#ifndef __SEARCH_H__
+#define __SEARCH_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
